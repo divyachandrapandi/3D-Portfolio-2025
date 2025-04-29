70b16532132bcd4315cae8d78da857df290d382a
divyachandrapandi <divya4shivalaya@gmail.com>
2025-04-29 14:53:58 +0530

    Added First committed code

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Hero from './sections/Hero.jsx';
+const App = () => {
+    return (
+     <>
+     <Hero />
+     </>
+    );
+};
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// @link: https://tailwindcss.com/docs/
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './src/**/*.{html,js,css,vue,ts,php,jsx,tsx}',
+        './src/js/*.{js,jsx,tsx}',
+        './src/css/*.{css,scss}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
