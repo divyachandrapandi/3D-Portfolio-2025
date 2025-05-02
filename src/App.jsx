6ee290c7a486a8d3657c8ddad156f2424ad5310d
divyachandrapandi <divya4shivalaya@gmail.com>
2025-05-02 14:08:28 +0530

    Created Page and Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import {Home, About, Projects, Contact } from "./pages";
 
 const App = () => {
     return (
-        <div className={"test"}>App</div>
+       <main className={"bg-slate-300/20"}>
+
+           <Router>
+               <Navbar/>
+               <Routes>
+                   <Route path={"/"} element={<Home />} />
+
+                   <Route path='/projects' element={<Projects />} />
+                   <Route path='/contact' element={<Contact />} />
+               </Routes>
+           </Router>
+
+       </main>
     );
 };
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home.jsx';
+import About from './About.jsx';
+import Projects from './Projects.jsx';
+import Contact from './Contact.jsx';
+
+
+export {Home, About, Projects, Contact};
