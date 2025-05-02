fca9a6de7c959f1939fbd6d13559d9bbd4feceba
divyachandrapandi <divya4shivalaya@gmail.com>
2025-05-02 18:01:57 +0530

    Added 3d Sky, Bird, Plane

---
+++ src/models/index.js
@@ -0,0 +1,10 @@
+import { Island } from './Island.jsx';
+import { Sky } from './Sky.jsx';
+import Plane from './Plane.jsx';
+import Bird from './Bird.jsx';
+export {
+    Island,
+    Sky,
+    Bird,
+    Plane
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader.jsx';
-import  {Island}  from '../models/Island.jsx';
+import { Island, Sky, Plane, Bird } from '../models';
+
 const Home = () => {
+
+    const adjustIslandForScreenSize = () => {
+        let screenScale = null;
+        let screenPosition = [0, -6.5, -43];
+
+        if (window.innerWidth < 768) {
+            screenScale = [0.9, 0.9, 0.9];
+        } else {
+            screenScale = [1, 1, 1];
+        }
+        return [screenScale, screenPosition];
+    };
+
+    const [islandScale, islandPosition] = adjustIslandForScreenSize();
+
     return (
         <section className="w-full h-screen relative">
             {/*<div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>*/ }
@@ -13,13 +29,20 @@ const Home = () => {
                     camera={ { near: 0.1, far: 1000 } }>
 
                 <Suspense fallback={ <Loader /> }>
-                    <directionalLight />
-                    <ambientLight />
-                    <pointLight />
-                    <spotLight />
-                    <hemisphereLight />
+                    <directionalLight position={ [1, 1, 1] } intensity={ 2 } />
+                    <ambientLight intensity={ 0.5 } />
+                    <pointLight position={ [10, 5, 10] } intensity={ 2 } />
 
-                    <Island/>
+                    <hemisphereLight
+                        skyColor="#b1e1ff"
+                        groundColor="#000000"
+                        intensity={ 1 }
+                    />
+                    <Sky />
+
+                    <Island position={ islandPosition }
+                            rotation={ [0.1, 4.7077, 0] }
+                            scale={ islandScale } />
                 </Suspense>
             </Canvas>
         </section>
