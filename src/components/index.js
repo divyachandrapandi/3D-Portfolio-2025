1e9de44f78abbe8ac679fb11e5a4c25e1c4a89f5
divyachandrapandi <divya4shivalaya@gmail.com>
2025-05-03 12:34:14 +0530

    About Skills, Experience Compeleted

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import HomeInfo from './HomeInfo.jsx';
 import Loader from './Loader.jsx';
 import Navbar from './Navbar.jsx';
 import Alert from './Alert.jsx';
+import CTA from './CTA.jsx';
 
 export {
     HomeInfo,
     Loader,
     Navbar,
-    Alert
+    Alert,
+    CTA
 }
